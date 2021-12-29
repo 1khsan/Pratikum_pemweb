fc763f6f75ffbc7a2c2aa9f61087645e8254ebca
1khsan <92222368+1khsan@users.noreply.github.com>
2021-12-29 14:45:17 +0700

    Add files via upload

---
+++ pratikum pekan-4/login/otentikasi.js
@@ -0,0 +1,11 @@
+
+      function login(){
+        let email = document.getElementById('e-mail');
+        let pw = document.getElementById("password");
+        if (email.value == "ahmad2017" && pw.value=="integrity"){
+            window.location.href="tampilan.html"
+        }
+        else{
+          alert("Maaf akun yang anda masukan salah, silahkan ulangi kembali")
+        };
+      }
